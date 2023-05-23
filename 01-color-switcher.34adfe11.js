db187b0df499bcfe7f6632bae761fb3d897b5149
yurchykyur <124770215+yurchykyur@users.noreply.github.com>
2023-05-23 07:32:21 +0000

    Deploying to gh-pages from @ yurchykyur/goit-js-hw-09@d3bcdc73f172f289185c1579dcb3a10309ebf79f 🚀

--- 01-color-switcher.34adfe11.js
+++
@@ -1,2 +0,0 @@
-!function(){var t={start:document.querySelector("button[data-start]"),stop:document.querySelector("button[data-stop]"),body:document.querySelector("body")},e=null;t.start.addEventListener("click",(function(o){t.start.setAttribute("disabled",""),e=setInterval((function(){var e="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0));t.body.style.backgroundColor=e}),1e3)})),t.stop.addEventListener("click",(function(o){clearInterval(e),t.start.removeAttribute("disabled")}))}();
-//# sourceMappingURL=01-color-switcher.34adfe11.js.map
